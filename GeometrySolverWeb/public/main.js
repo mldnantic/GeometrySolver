a373e6ce96a83f32eda6f2c5eb3856b86a1dfe26
Djomla <m.antic@elfak.rs>
2023-12-24 02:03:29 +0100

    webgl trougao crveni

--- GeometrySolverWeb/public/main.js
+++ GeometrySolverWeb/public/main.js
@@ -8,27 +8,50 @@ if(!gl)
 
 alert("Dobrodosli u GeometrySolver :D");
 
+
 const vertexData = [
     0,1,0,
     1,-1,0,
     -1,-1,0,
 ];
 
-const buffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+const colorData = [
+    1, 0, 0,
+    0, 1, 0,
+    0, 0, 1,
+];
+
+const positionBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
+
+const colorBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colorData), gl.STATIC_DRAW);
 
 const vertexShader = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(vertexShader, `
+precision mediump float;
+
 attribute vec3 position;
+attribute vec3 color;
+varying vec3 vColor;
+
+uniform mat4 matrix;
+
 void main(){
-    gl_Position = vec4(position, 1);
+    vColor = color;
+    gl_Position = matrix * vec4(position, 1);
 }
 `);
 gl.compileShader(vertexShader);
 
 const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 gl.shaderSource(fragmentShader,`
+precision mediump float;
+
+varying vec3 vColor;
+
 void main(){
     gl_FragColor = vec4(1, 0, 0, 1);
 }
@@ -47,7 +70,24 @@ if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
 
 const positionLocation = gl.getAttribLocation(program, `position`);
 gl.enableVertexAttribArray(positionLocation);
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
 
+const colorLocation = gl.getAttribLocation(program, `color`);
+gl.enableVertexAttribArray(colorLocation);
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0);
+
 gl.useProgram(program);
+
+// const uniformLocations = {
+//     matrix: gl.getUniformLocation(program,`matrix`),
+// };
+
+
+// const matrix = mat4.create();
+// mat4.translate(matrix,matrix,[.3,.2,0]);
+
+// gl.uniformMatrix4fv(uniformLocations.matrix, false, matrix);
+
 gl.drawArrays(gl.TRIANGLES, 0, 3);
